b16080789a90c8819fa884668df55199605b26c4
JoshHyde9 <joshhyde546@gmail.com>
2020-03-31 11:02:46 +1100

    Created Task component

--- src/components/layout/Content.js
+++ src/components/layout/Content.js
@@ -2,11 +2,13 @@ import React from "react";
 
 // Components
 import { Sidebar } from "./Sidebar";
+import { Tasks } from "../Tasks";
 
 export const Content = () => {
   return (
-    <section>
+    <section className="content">
       <Sidebar />
+      <Tasks />
     </section>
   );
 };
