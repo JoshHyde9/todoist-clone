938f0c20c675a7ab6fa6dcfd5d79378f2e0963df
JoshHyde9 <joshhyde546@gmail.com>
2020-03-31 14:11:10 +1100

    Tasks now display to UI

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 // Components
 import { Checkbox } from "./Checkbox";
+
+// Constants
+import { collatedTasks } from "../constants";
+
+// Context
+import { useSelectedProjectValue, useProjectsValue } from "../context";
+
+// Helpers
+import { getTitle, getCollatedTitle, collatedTasksExist } from "../helpers";
+
+// Hooks
 import { useTasks } from "../hooks";
 
 export const Tasks = () => {
-  const { tasks } = useTasks("1");
+  const { selectedProject } = useSelectedProjectValue();
+  const { projects } = useProjectsValue();
+  const { tasks } = useTasks(selectedProject);
 
   let projectName = "";
+
+  if (projects && selectedProject && !collatedTasksExist(selectedProject)) {
+    projectName = getTitle(projects, selectedProject).name;
+  }
+
+  if (collatedTasksExist(selectedProject) && selectedProject) {
+    projectName = getCollatedTitle(collatedTasks, selectedProject).name;
+  }
+
+  useEffect(() => {
+    document.title = `${projectName}: Todoist Clone`;
+  });
 
   return (
     <div className="tasks" data-testid="tasks">
